d3dbb02c36ebe21092afd26dc9733cc2ae140eda
Shamsher Singh <shamsher.singh400@gmail.com>
2020-03-27 04:21:59 +0530

    remoe module

--- bngvoiceotp/src/main/java/com/bng/bngvoiceotp/BngVoiceOtpCallBack.java
+++
@@ -1,9 +0,0 @@
-package com.bng.bngvoiceotp;
-
- public interface BngVoiceOtpCallBack {
-    public void initialize(String status);
-    public void success(String number, String status);
-    public void failure(String reason);
-    public void error(String error);
-    public void deInitialize(String message);
-}
--- bngvoiceotp/src/main/java/com/bng/bngvoiceotp/CallState.java
+++
@@ -1,7 +0,0 @@
-package com.bng.bngvoiceotp;
-
-import android.telephony.TelephonyManager;
-
- interface CallState {
-    void callState(String state);
-}
